0a8e9fa5db0723b659761be04a822f299a6bb943
Karuna200 <83528433+Karuna200@users.noreply.github.com>
2022-02-10 12:26:40 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -120,7 +120,7 @@ async def next_page(bot, query):
 async def advantage_spoll_choker(bot, query):
     _, user, movie_ = query.data.split('#')
     if int(user) != 0 and query.from_user.id != int(user):
-        return await query.answer("**Babe❤️, Don't Tap Others Request,Request On Your Own**", show_alert=True)
+        return await query.answer("Babe❤️, Don't Tap Others Request,Request On Your Own", show_alert=True)
     if movie_ == "close_spellcheck":
         return await query.message.delete()
     movies = SPELL_CHECK.get(query.message.reply_to_message.message_id)
