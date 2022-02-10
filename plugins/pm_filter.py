f24e4a4fbf559f6357c02d22364ea1f832962e6c
Karuna200 <83528433+Karuna200@users.noreply.github.com>
2022-02-10 12:21:56 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -135,7 +135,7 @@ async def advantage_spoll_choker(bot, query):
             k = (movie, files, offset, total_results)
             await auto_filter(bot, query, k)
         else:
-            k = await query.message.edit('**This Movie Is Not Available In Public Group❌, \nIf It Is Released In Digital Platform, You Will Get It In VIP Group 🌟**')
+            k = await query.message.edit('**This Movie Is Not Available In Public Group❌, \nIf It Is Released In Digital Platform, You Will Get It In @TVSERIESCW_VIP 🌟**')
             await asyncio.sleep(10)
             await k.delete()
 
