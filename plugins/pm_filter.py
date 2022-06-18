fb503f8d7d6975079c4b0f7d9c63308cb6390833
Karuna200 <83528433+Karuna200@users.noreply.github.com>
2022-06-18 10:55:56 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -745,7 +745,6 @@ async def auto_filter(client, msg, spoll=False):
             poster = pic.replace('.jpg', "._V1_UX360.jpg")
             await message.reply_photo(photo=poster, caption=cap[:1024], reply_markup=InlineKeyboardMarkup(btn))
         await asyncio.sleep(300)
-            await hmm.delete()
             await message.delete()
 except Exception as e:
             logger.exception(e)
